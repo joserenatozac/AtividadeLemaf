ed5a9569422d0007dae1e08c038fe7e1531897d0
José Renato <jose.renato@dtidigital.com.br>
2018-04-04 21:27:18 -0300

    Inicio implementação do projeto

---
+++ AtividadeLemafJoseRenato/AtividadeLemafJoseRenato.Batch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeLemafJoseRenato.Batch
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("Entrada.txt");
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            string[] lines = File.ReadAllLines(@"Entrada.txt");
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+        }
+    }
+}
---
+++ AtividadeLemafJoseRenato/AtividadeLemafJoseRenato.Fronteiras/Dtos/AgendamentoDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fronteira.Dtos
+{
+    public class AgendamentoDto
+    {
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public int QuantidadePessoas { get; set; }
+        public bool NecessitaAcessoInternet { get; set; }
+        public bool NecessitaTvEWebcam { get; set; }
+    }
+}
