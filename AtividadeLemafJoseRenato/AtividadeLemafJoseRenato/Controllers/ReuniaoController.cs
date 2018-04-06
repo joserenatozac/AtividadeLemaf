7bb1887df5c8c6ec23b9e4f6932dd4eb1fa2efd5
José Renato <jose.renato@dtidigital.com.br>
2018-04-06 05:26:48 -0300

    Inicio Projeto Web

---
+++ AtividadeLemafJoseRenato/AtividadeLemafJoseRenato/Controllers/ReuniaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AtividadeLemafJoseRenato.Controllers
+{
+    public class ReuniaoController : Controller
+    {
+        // GET: Reuniao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
